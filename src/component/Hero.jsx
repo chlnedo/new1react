2b6cfb2bcaa58d80fb1262f4f35cdd9c80e1533e
chlnedo <ankittyagi95288@gmail.com>
2023-08-12 17:14:23 +0530

    reactfirst

---
+++ src/component/Hero.jsx
@@ -0,0 +1,41 @@
+
+
+  function Hero(){
+
+   return(
+
+  <main className="hero">
+   
+    <div className="hero-content">
+    <h1>Your Feet Deserve Best</h1>
+    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+    <div className="hero-btn">
+        <button>SHOP NOW</button>
+        <button className="secondary-btn">CATEGORY</button>
+    </div>
+
+    <div className="shopping">
+        <p>Also Available On</p>
+        <div className="brand-logo">
+            <img src="amazon.png" alt ="logo" />
+            <img src="flipkart.png" alt ="logo" />
+        </div>
+    </div>
+
+
+
+    </div>
+
+    <div className="hero-image">
+        <img src="shoe_image.png" alt="" />
+
+    </div>
+
+  </main>
+
+   );
+
+  };
+
+  export default Hero;
---
+++ src/component/Item.js
@@ -0,0 +1,15 @@
+
+function Item({title}){
+  return(
+   <div>
+      <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+    <div>{title}</div>
+   </div>
+  );
+
+}
+
+export default Item;
